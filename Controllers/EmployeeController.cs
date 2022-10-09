ea7d6068186a3f485336f89bf82c0a93bf8f45e0
Amila Prageeth <amila.perumbuli.work@gmail.com>
2022-10-09 14:43:01 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using Assignment.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Assignment.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ILogger<EmployeeController> _logger;
+
+        public EmployeeController(ILogger<EmployeeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace Assignment.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime StartDate { get; set; }
+    }
+}
